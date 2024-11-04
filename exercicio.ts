3aa26a96283483a97ffcd0c1642605480596fbfd
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-11-04 16:46:59 -0300

    Update exercicio.ts

--- exercicio.ts
+++ exercicio.ts
@@ -4,42 +4,23 @@ const numero2 = document.getElementById('n2') as HTMLInputElement
 const botaoResulatado = document.getElementById('botaoMult')
 const resposta = document.getElementById('resultadoDaMulti')
 
-
-function Multiplicar(valor1: number, valor2: number): void{
-    if (isNaN(valor1) || isNaN(valor2)){
-        resposta?.setAttribute('id', 'resultadoDaMulti--is-open'),
-        resposta!.innerHTML = `<p> Não existe valor para ser Multiplicado, Digite 2 números</p>`
-    } else{
-        const resultado = valor1 * valor2
-
-        resposta?.setAttribute('id', 'resultadoDaMulti--is-open'),
-        resposta!.innerHTML = `<p>O resultado da sua multiplicação é ${resultado}</p>`
-        
-    }
-}
-
-function DizerNome(nome: string){
-  
-    
-    if ( nome.trim() === ''){
-        resultadoDoNome?.setAttribute('id ', 'resultadoDaMulti--is-open')
-        resultadoDoNome!.innerHTML = '<p>Digite algo na caixa de texto!</p>'
-    } else {
-        resultadoDoNome?.setAttribute('id ', 'resultadoDaMulti--is-open')
-        resultadoDoNome!.innerHTML = `<p>Olá, ${nome}, como vai?</p>`
-    }
-
-}
-
-
-
-
 botaoResulatado?.addEventListener('click', (e: MouseEvent) =>{
 
     const valor1 = parseFloat(numero1.value)
     const valor2 = parseFloat(numero2.value)
 
+    function Multiplicar(valor1: number, valor2: number): void{
+        if (isNaN(valor1) || isNaN(valor2)){
+            resposta?.setAttribute('id', 'resultadoDaMulti--is-open'),
+            resposta!.innerHTML = `<p> Não existe valor para ser Multiplicado, Digite 2 números</p>`
+        } else{
+            const resultado = valor1 * valor2
     
+            resposta?.setAttribute('id', 'resultadoDaMulti--is-open'),
+            resposta!.innerHTML = `<p>O resultado da sua multiplicação é ${resultado}</p>`
+            
+        }
+    }
 
     Multiplicar(valor1, valor2)
 })
@@ -53,7 +34,18 @@ const resultadoDoNome = document.getElementById('resultadoDoNome')
 botaoDoNome?.addEventListener('click', (e: MouseEvent) => {
 
     const nome: string = seuNome.value
+    function DizerNome(nome: string){
+  
     
+        if ( nome.trim() === ''){
+            resultadoDoNome?.setAttribute('id ', 'resultadoDaMulti--is-open')
+            resultadoDoNome!.innerHTML = '<p>Digite algo na caixa de texto!</p>'
+        } else {
+            resultadoDoNome?.setAttribute('id ', 'resultadoDaMulti--is-open')
+            resultadoDoNome!.innerHTML = `<p>Olá, ${nome}, como vai?</p>`
+        }
+
+    }
     DizerNome('nome')
 }) 
 
